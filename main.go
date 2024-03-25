9a7272401f8b35e43e8d84345c86e11f8a8b6b51
MRTJ\afaaza <afaaza@jakartamrt.co.id>
2024-03-25 11:48:17 +0700

    try change port

--- main.go
+++ main.go
@@ -53,8 +53,8 @@ func main() {
 	r.HandleFunc("/socialmedias/{socialMediaID}", handlers.DeleteSocialMediaEntryByID).Methods("DELETE")
 
 	// Start server
-	fmt.Println("Server started at localhost:8080")
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Println("Server started at localhost:80")
+	log.Fatal(http.ListenAndServe(":80", r))
 }
 
 func welcomeMessage(w http.ResponseWriter, r *http.Request) {
